232ede484c8311218edb26525ec0bb8a48285f1a
Ana C Sanchez <ana.sanchez6547@gmail.com>
2019-01-21 22:45:32 -0500

    completed get and post routes to create shorturl and redirect

--- server/api/shorturl.js
+++ server/api/shorturl.js
@@ -1,6 +1,20 @@
 const router = require('express').Router()
+const { URL } = require('../db/models')
 module.exports = router;
 
-router.post('/new', (req,res) => {
+router.get('/:id', (req,res) => {
+  URL.findById(req.params.id)
+  .then(foundUrl => res.redirect(foundUrl.original))
+  .catch(() => res.send("short url does not exist"))
+})
 
+router.post('/new', (req,res) => {
+  if (req.body.url) {
+    URL.create({ original: req.body.url })
+    .then(newUrl => res.send({"original_url": newUrl.original, "short_url": newUrl.id }))
+    .catch(err => console.log(err))
+  }
+  else {
+    res.end()
+  }
 })
